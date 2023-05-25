9534501b56a23697b6a73dbfb5320fda3177015e
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-05-25 08:13:05 +0300

    upload

--- ex_01.py
+++ ex_01.py
@@ -8,12 +8,12 @@ lst2 = [1, 2, 3, 4]
 print(type(lst1))
 
 
-for i in range(10):
+for i in range(10, 20):
     lst1.append(i)
     
-print(lst1) 
+print(lst1[3]) 
 
-print(lst1[0])   
+# print(lst1[0])   
 
-for i in range(len(lst1)):
-    print(lst1[i])
+for index, item in enumerate(lst1):
+    print( index, item)
